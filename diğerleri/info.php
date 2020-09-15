7e65911692a9e3edc6ad1c3ed95fb86bc7cd8d4f
umutdenizpro <68480298+umutdenizpro@users.noreply.github.com>
2020-09-15 13:41:12 +0300

    Create info.php

---
+++ diğerleri/info.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width">
+</head>
+<body>
+<h1>Hakkında </h1>
+=
+Geliştiriciler: Umut Deniz, SeCone
+Programlama Dilleri:HTML, css, php, java, json, xml.
+Çıkış Tarihi:2020
+ 
+---------
+<a>
+Lisans: SeCone
+tür:pixel art sitesi
+= 
+Bu siteyi beğendiniz mi?
+</a>
+<code>{Başarılı olmak iyidir.} </code>
+</body>
+</html>
