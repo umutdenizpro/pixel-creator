3d341db386705e84e18786d1d69ba48cdbc871c9
umutdenizpro <68480298+umutdenizpro@users.noreply.github.com>
2020-09-18 11:10:41 +0300

    Create iletişim.php

---
+++ diğerleri/2/iletişim.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width">
+</head>
+<body>
+	<h3>İletişim </h3>
+	<font face="verdana" color="pink">
+	<p>Eposta:elacone399@outlook.com.tr</p>
+	</font>
+	<a>
+	<font color="pink" face="verdana">
+	Whatsapp/TEL: 0530 517 98 33
+	</font>
+	<font color="pink" face="verdana">
+	<p>Github: UmutDenizPro </p>
+	<a>Site: secone.tgt.repl.co </a>
+	</font>
+	</body>
+	</html>
